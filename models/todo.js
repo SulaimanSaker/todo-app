0d654d32d0539fa2cded64800fe4c5efb2a963ef
Sulaiman Saker <sulaiman.saker@hotmail.com>
2021-07-01 08:00:36 +0300

    Add Todo model

---
+++ models/todo.js
@@ -0,0 +1,35 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Todo = mongoose.model(
+  'Todos',
+  new mongoose.Schema({
+    text: {
+      type: String,
+      required: [true, 'You should enter some text'],
+      minlength: [5, 'Text should be at least 5 characters'],
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+  })
+);
+
+function validateTodo(todo) {
+  const schema = {
+    text: Joi.string().min(5).max(50).required(),
+    completed: Joi.Boolean(),
+    creator: Joi.objectId().required(),
+  };
+
+  return Joi.validate(todo, schema);
+}
+
+exports.Todo = Todo;
+exports.validate = validateTodo;
